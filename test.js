10de5dfb13bd3e4de68596435c03d6ec03584888
Webdeveloperblr <kdv3505381@gmail.com>
2018-09-07 17:41:26 +0300

    WIP, works, but ugly code :(

--- test.js
+++ test.js
@@ -9,16 +9,16 @@ var batch = [{ acceleration: 0.2624134837985052, timestamp: 3114.900000015041, t
     { acceleration: -0.07157262492286878, timestamp: 3154.90000002319, type: 'MOUSE_ACCELERATION' },
     { timestamp: 3162.9000000248197, type: 'MOUSE_MOVE', x: 3, y: 1 },
     { timestamp: 3162.9000000248197, type: 'MOUSE_MOVE', x: 4, y: 0 },
+    { timestamp: 3162.9000000248197, type: 'MOUSE_MOVE', x: 144, y: -140 },
+    { timestamp: 3162.9000000248197, type: 'MOUSE_MOVE', x: 183, y: -140 },
     { timestamp: 3162.9000000248197, type: 'MOUSE_VELOCITY', velocity: 0.8838834763031137 },
     { acceleration: -0.22139439798905297, timestamp: 3162.9000000248197, type: 'MOUSE_ACCELERATION' },
 ];
 ;
 ;
+var mouseMoves = batch.filter(function (val) { return val.type === 'MOUSE_MOVE'; });
 var isOnLine = function (pointer, fnArgs) {
     return fnArgs.A * pointer.x + fnArgs.B * pointer.y + fnArgs.C === 0;
-};
-var getCompareVal = function (pointer1, pointer2) {
-    return Math.abs(((pointer1.x - pointer2.x) / (pointer1.y - pointer2.y)));
 };
 var getVars = function (pointer1, pointer2) {
     return {
@@ -30,21 +30,31 @@ var getVars = function (pointer1, pointer2) {
 var getDistance = function (firstPointer, lastPointer) {
     return Math.abs(Math.sqrt(Math.pow((lastPointer.x - firstPointer.x), 2) + Math.pow((lastPointer.x - firstPointer.x), 2)));
 };
-var ifOnLine = (function () {
-    var arr = batch.filter(function (val) { return val.type === 'MOUSE_MOVE'; });
-    console.log(arr.reduce(function (acc, el) {
+var cb = function (pointerLine) {
+    console.log("Bot detected! Trace: " + pointerLine.lineElements + "; Distance: " + pointerLine.distance);
+};
+var math = (function (cb) {
+    mouseMoves.reduce(function (acc, el) {
         if (acc.lineElements.length > 1) {
-            isOnLine(el, acc.fnArgs) ? acc.lineElements.push(el) : acc = {
-                lineElements: [el]
-            };
-            console.log(getDistance(acc.lineElements[0], acc.lineElements[acc.lineElements.length - 1]));
-            return acc;
+            if (isOnLine(el, acc.fnArgs)) {
+                acc.lineElements.push(el);
+            }
+            else {
+                acc.distance > 100 ? cb(Object.assign({}, acc)) : null;
+                acc = { lineElements: [el] };
+            }
         }
-        acc.lineElements.push(el);
-        acc.lineElements.length === 2 ?
-            acc.fnArgs = getVars(acc.lineElements[0], acc.lineElements[1]) : null;
+        else {
+            acc.lineElements.push(el);
+            acc.lineElements.length === 2 ?
+                acc.fnArgs = getVars(acc.lineElements[0], acc.lineElements[1]) : null;
+        }
+        if (acc.lineElements.length >= 2) {
+            acc.distance += getDistance(acc.lineElements[acc.lineElements.length - 2], acc.lineElements[acc.lineElements.length - 1]);
+        }
         return acc;
     }, {
-        lineElements: []
-    }));
-})();
+        lineElements: [],
+        distance: 0
+    });
+})(cb);
--- test.ts
+++ test.ts
@@ -7,6 +7,8 @@ const batch: any[] = [{ acceleration: 0.2624134837985052, timestamp: 3114.900000
     { acceleration: -0.07157262492286878, timestamp: 3154.90000002319, type: 'MOUSE_ACCELERATION' },
     { timestamp: 3162.9000000248197, type: 'MOUSE_MOVE', x: 3, y: 1 },
     { timestamp: 3162.9000000248197, type: 'MOUSE_MOVE', x: 4, y: 0 },
+    { timestamp: 3162.9000000248197, type: 'MOUSE_MOVE', x: 144, y: -140 },
+    { timestamp: 3162.9000000248197, type: 'MOUSE_MOVE', x: 183, y: -140 },
     { timestamp: 3162.9000000248197, type: 'MOUSE_VELOCITY', velocity: 0.8838834763031137 },
     { acceleration: -0.22139439798905297, timestamp: 3162.9000000248197, type: 'MOUSE_ACCELERATION' },
 ];
@@ -23,44 +25,7 @@ export interface InterfacePoint extends InterfacePackage {
     y: number;
 };
 
-export interface ILine {
-    compareVal?: number;
-    lineElements: InterfacePoint[];
-}
-
-export interface IComparer {
-    value: number;
-    pointer: InterfacePoint;
-}
-
-/*const comparePointer = (pointer: InterfacePoint, comparer: IComparer): boolean =>
-    getCompareVal(pointer, comparer.pointer) === comparer.value;
-
-const getCompareVal = (pointer1: InterfacePoint, pointer2: InterfacePoint): number =>
-    Math.abs(((pointer1.x - pointer2.x) / (pointer1.y - pointer2.y)));
-
-const ifOnLine = (() => {
-
-    const arr = batch.filter(val => val.type === 'MOUSE_MOVE');
-
-    console.log(arr.reduce((acc: ILine, el) => {
-
-        if (acc.lineElements.length > 1) {
-            comparePointer(el, { pointer: acc.lineElements[0], value: acc.compareVal })
-                ? acc.lineElements.push(el) : acc.lineElements = [el];
-            return acc;
-        } else {
-            acc.lineElements.push(el);
-            acc.lineElements.length === 2 ?
-                acc.compareVal = getCompareVal(acc.lineElements[0], acc.lineElements[1]) : 0;
-        }
-        return acc;
-
-    }, {
-        lineElements: []
-    }));
-
-})();*/
+const mouseMoves = batch.filter(val => val.type === 'MOUSE_MOVE');
 
 export interface IFnArgs {
     A: number;
@@ -68,7 +33,7 @@ export interface IFnArgs {
     C: number;
 }
 
-export interface NEWILine {
+export interface ILine {
     fnArgs?: IFnArgs;
     distance?: number;
     lineElements: InterfacePoint[];
@@ -76,9 +41,6 @@ export interface NEWILine {
 
 const isOnLine = (pointer: InterfacePoint, fnArgs: IFnArgs): boolean =>
     fnArgs.A * pointer.x + fnArgs.B * pointer.y + fnArgs.C === 0;
-
-const getCompareVal = (pointer1: InterfacePoint, pointer2: InterfacePoint): number =>
-    Math.abs(((pointer1.x - pointer2.x) / (pointer1.y - pointer2.y)));
 
 const getVars = (pointer1: InterfacePoint, pointer2: InterfacePoint): IFnArgs => {
     return {
@@ -91,28 +53,36 @@ const getVars = (pointer1: InterfacePoint, pointer2: InterfacePoint): IFnArgs =>
 const getDistance = (firstPointer: InterfacePoint, lastPointer: InterfacePoint): number =>
     Math.abs(Math.sqrt(Math.pow((lastPointer.x - firstPointer.x), 2) + Math.pow((lastPointer.x - firstPointer.x), 2)));
 
-const math = (() => {
+const cb = (pointerLine: ILine): void => {
+    console.log(`Bot detected! Trace: ${ pointerLine.lineElements }; Distance: ${ pointerLine.distance }`);
+};
 
-    const arr = batch.filter(val => val.type === 'MOUSE_MOVE');
+const math = ((cb) => {
 
-    console.log(arr.reduce((acc: NEWILine, el) => {
+    mouseMoves.reduce((acc: ILine, el) => {
 
         if (acc.lineElements.length > 1) {
-            isOnLine(el, acc.fnArgs) ? acc.lineElements.push(el) : acc = {
-                    lineElements: [el]
-                };
-            console.log(getDistance(acc.lineElements[0], acc.lineElements[acc.lineElements.length - 1]));
 
+            if (isOnLine(el, acc.fnArgs)) {
+                acc.lineElements.push(el);
+            } else {
+                acc.distance > 100 ? cb((<any>Object).assign({}, acc)) : null;
+                acc = { lineElements: [el] };
+            }
 
-            return acc;
+        } else {
+            acc.lineElements.push(el);
+            acc.lineElements.length === 2 ?
+                acc.fnArgs = getVars(acc.lineElements[0], acc.lineElements[1]) : null;
         }
-        acc.lineElements.push(el);
-        acc.lineElements.length === 2 ?
-            acc.fnArgs = getVars(acc.lineElements[0], acc.lineElements[1]) : null;
+        if (acc.lineElements.length >= 2) {
+            acc.distance += getDistance(acc.lineElements[acc.lineElements.length - 2], acc.lineElements[acc.lineElements.length - 1]);
+        }
 
         return acc;
     }, {
-        lineElements: []
-    }));
+        lineElements: [],
+        distance: 0
+    });
 
-})();
+})(cb);
